1cb84697a161c1a2bf445e5635d88c48f343d178
Caio Ribeiro <caioalvribeiro@gmail.com>
2025-08-29 16:56:50 -0300

    carrossel funcional

--- components/carrossel/carrossel.tsx
+++ components/carrossel/carrossel.tsx
@@ -1,21 +1,56 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import Produto from "../produto/produto";
+import useEmblaCarousel from "embla-carousel-react";
 
 export default function Carrossel() {
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: true,
+        slidesToScroll: 1, 
+        breakpoints: {
+            '(min-width: 768px)': { slidesToScroll: 2 }, 
+            '(min-width: 1280px)': { slidesToScroll: 1 }  
+        },
+    })
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    const [scrollSnaps, setScrollSnaps] = useState<number[]>([])
+
+    useEffect(() => {
+        if (!emblaApi) return
+
+        setScrollSnaps(emblaApi.scrollSnapList())
+        emblaApi.on("select", () => setSelectedIndex(emblaApi.selectedScrollSnap()))
+        emblaApi.on("reInit", () => setSelectedIndex(emblaApi.selectedScrollSnap()))
+    }, [emblaApi])
+
     return (
         <div className="px-5 w-full">
             <div className="flex flex-col px-2.5 py-7.5 gap-5 md:gap-7.5 w-full items-center justify-center bg-cinza/60 rounded-xl">
                 <h1 className="text-dourado text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl 3xl:text-7xl font-semibold text-center font-cormorant-sc">Veja Nossos Destaques</h1>
+
                 {/*Carrossel*/}
-                <div className="w-full flex flex-col  items-center justify-center py-2.5 gap-7.5 md:flex-row">
-                    <i className="bi bi-chevron-compact-up text-dourado text-4xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl 3xl:text-9xl md:rotate-270"></i>
-                    <div className="flex flex-col items-center justify-center gap-15 md:gap-19 md:flex-row">
-                        <Produto temDesc={false} />
-                        <Produto temDesc={false} />
-                        <div className="hidden lg:block">
-                            <Produto temDesc={false} />
+                <div className="w-full flex items-center justify-center py-1.5 gap-0 bp-540:gap-7.5">
+
+                    {/*Botão de voltar*/}
+                    <i className="bi bi-chevron-compact-left text-dourado text-2xl bp-540:text-5xl sm:text-3xl md:text-6xl xl:text-7xl 2xl:text-8xl 3xl:text-9xl cursor-pointer" onClick={() => emblaApi?.scrollPrev()}></i>
+
+                    {/*Container com os cards*/}
+                    <div className="overflow-hidden max-w-6xl" ref={emblaRef}>
+                        <div className="flex">
+                            {[1, 2, 3, 4, 5, 6].map((n) => (
+                                <div
+                                    key={n}
+                                    className="flex flex-[0_0_100%] md:flex-[0_0_50%] lg:flex-[0_0_33.33%] p-5 items-center justify-center w-fit"
+                                >
+                                    <Produto temDesc={false} />
+                                </div>
+                            ))}
                         </div>
                     </div>
-                    <i className="bi bi-chevron-compact-down text-dourado text-4xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl 3xl:text-9xl md:rotate-270"></i>
+
+                    {/*Botão de avançar*/}
+                    <i className="bi bi-chevron-compact-right text-dourado text-2xl sm:text-3xl bp-540:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl 3xl:text-9xl cursor-pointer" onClick={() => emblaApi?.scrollNext()}></i>
                 </div>
             </div>
         </div>
